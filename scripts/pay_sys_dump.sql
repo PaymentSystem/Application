91db2c18d4870e7634bc079c135f642846efcf57
Daniil9596 <daniil.shakurov95@gmail.com>
2018-10-17 18:59:37 +0300

    Fixed sql script for Windows compatibility and extended restore db script for MacOS

--- scripts/pay_sys_dump.sql
+++ scripts/pay_sys_dump.sql
@@ -19,7 +19,7 @@ DROP DATABASE IF EXISTS payment_system;
 -- Name: payment_system; Type: DATABASE; Schema: -; Owner: postgres
 --
 
-CREATE DATABASE payment_system WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+CREATE DATABASE payment_system WITH TEMPLATE = template0;
 
 
 ALTER DATABASE payment_system OWNER TO postgres;
