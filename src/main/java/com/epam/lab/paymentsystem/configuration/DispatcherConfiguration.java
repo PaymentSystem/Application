2049c430dd5920b457774f960b1430f56774f233
LicenserX <licenset@gmail.com>
2018-10-28 19:13:07 +0300

    Has been added addInterceptors

--- src/main/java/com/epam/lab/paymentsystem/configuration/DispatcherConfiguration.java
+++ src/main/java/com/epam/lab/paymentsystem/configuration/DispatcherConfiguration.java
@@ -4,20 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 @ComponentScan("com.epam.lab.paymentsystem")
 @EnableWebMvc
-public class DispatcherConfiguration {
+public class DispatcherConfiguration implements WebMvcConfigurer {
     private static final String PREFIX = "/WEB-INF/jsp/";
     private static final String SUFFIX = ".jsp";
 
     @Bean
     public ViewResolver internalResourceViewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+         InternalResourceViewResolver bean = new InternalResourceViewResolver();
         bean.setPrefix(PREFIX);
         bean.setSuffix(SUFFIX);
         return bean;
@@ -29,5 +36,20 @@ public class DispatcherConfiguration {
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("en"));
+        localeResolver.setCookieName("myLocaleName");
+        localeResolver.setCookieMaxAge(4800);
+        return localeResolver;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
