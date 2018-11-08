5dc2cbe00d2d1e4a9bd656c863276def19fc0377
Daniil9596 <daniil.shakurov95@gmail.com>
2018-11-08 22:30:32 +0300

    Fix autowired

--- src/main/java/com/epam/lab/paymentsystem/controller/CardController.java
+++ src/main/java/com/epam/lab/paymentsystem/controller/CardController.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CardController {
-  @Autowired private UserService userService;
+  @Autowired
+  private UserService userService;
 
-  @Autowired private CardService cardService;
+  @Autowired
+  private CardService cardService;
 
   private static final Logger LOGGER = LogManager.getLogger(AccountController.class);
   private static final String ADD_CARD_PAGE = "addCard";
