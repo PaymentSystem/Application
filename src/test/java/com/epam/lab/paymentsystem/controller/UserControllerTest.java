b3857107d76869943c27a809a76cfc062d064dc6
Dmitrii_Ananev <Licensent@gmail.com>
2018-11-09 22:03:19 +0300

    try to do tests

--- src/test/java/com/epam/lab/paymentsystem/controller/UserControllerTest.java
+++ src/test/java/com/epam/lab/paymentsystem/controller/UserControllerTest.java
@@ -1,10 +1,89 @@
 package com.epam.lab.paymentsystem.controller;
 
+import com.epam.lab.paymentsystem.configuration.DispatcherConfiguration;
+import com.epam.lab.paymentsystem.configuration.TestApplicationConfiguration;
+import com.epam.lab.paymentsystem.entities.User;
+import com.epam.lab.paymentsystem.repository.UserRepository;
+import javax.servlet.ServletContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+    TestApplicationConfiguration.class,
+    DispatcherConfiguration.class
+})
+@WebAppConfiguration
 public class UserControllerTest {
 
+  @Autowired
+  private WebApplicationContext wac;
+
+  private MockMvc mockMvc;
+
+  @BeforeEach
+  public void setUp() {
+    Authentication authentication = mock(Authentication.class);
+    SecurityContext securityContext = mock(SecurityContext.class);
+    when(securityContext.getAuthentication()).thenReturn(authentication);
+    SecurityContextHolder.setContext(securityContext);
+    MockitoAnnotations.initMocks(this);
+    mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+  }
+
   @Test
-  public void testAddUser() {
+  public void givenWac_whenServletContext_thenItProvidesGreetController() {
+    ServletContext servletContext = wac.getServletContext();
+
+    assertNotNull(servletContext);
+    assertTrue(servletContext instanceof MockServletContext);
+    assertNotNull(wac.getBean(UserController.class));
+  }
+
+  @Test
+  public void testAddUser() throws Exception {
+    mockMvc.perform(get("/")).andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("index"))
+        .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"));
+  }
+
+  @Autowired
+  private UserRepository studentRepository;
+
+  @Test
+  public void givenStudent_whenSave_thenGetOk() {
+
+    User student = new User("test", "test", "test8", null);
+    //student.setId(1);
+    studentRepository.save(student);
+
+    User student2 = studentRepository.getUserByLogin("test8");
+    assertEquals("test8", student2.getLogin());
   }
 }
+
